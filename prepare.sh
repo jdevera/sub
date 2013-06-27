d2c4101579cf3500edf40d14057bb3e73b77ded1
Jacobo de Vera <devel@jacobodevera.com>
2013-06-27 13:00:16 +0100

    Adjust prepare script to work with new structure
    Make sure the prepare script does not change the contents of the main
    sub script

--- prepare.sh
+++ prepare.sh
@@ -1,40 +1,42 @@
 #!/usr/bin/env bash
-set -e
 
-create_sub() {
-  file="$1"
-  SUBNAME="$2"
-  ENVNAME="$3"
-  sed "s/sub/$SUBNAME/g;s/SUB_ROOT/$ENVNAME/g" "$file" > $(echo $file | sed "s/sub/$SUBNAME/g")
-  rm $file
+
+chmod_files()
+{
+    for file in "$1"/*; do
+        [[ ! -d $file ]] && chmod +x "$file"
+    done
 }
 
-chmod_files() {
-  for file in $1/*; do
-    chmod a+x $file
-  done
+make_new_path()
+{
+    local path="$1"
+    local dir="$(dirname "$path")"
+    local base="$(basename "$path" | sed "s/^sub/${SUBNAME}/g")"
+    echo "$dir/$base"
 }
 
 prepare_sub() {
-  SUBNAME="$2"
-  mkdir -p $(echo $1 | sed "s/sub/$SUBNAME/g")
+    local path="$1"
+    local newpath="$(make_new_path "$path")"
 
-  for file in $1/sub*; do
-    if [ -d "$file" ]; then
-      prepare_sub $file $SUBNAME
-    elif [ -f "$file" ]; then
-      create_sub $file $SUBNAME $ENVNAME
+    if [[ -d $path ]]; then
+        for file in "$path"/sub-*; do
+            prepare_sub "$file"
+        done
+        chmod_files "$path"
+    elif [[ -f $path ]]; then
+        sed -i "s/\bsub\b/$SUBNAME/g;s/\b_SUB_ROOT\b/_$ENVNAME/g" "$path"
     fi
-  done
 
-  chmod_files $(echo $1 | sed "s/sub/$SUBNAME/g")
-  rm -rf $1
+    mv "$path" "$newpath"
 }
 
+
 NAME="$1"
-if [ -z "$NAME" ]; then
-  echo "usage: prepare.sh NAME_OF_YOUR_SUB" >&2
-  exit 1
+if [[ -z $NAME ]]; then
+    echo "usage: prepare.sh NAME_OF_YOUR_SUB" >&2
+    exit 1
 fi
 
 SUBNAME=$(echo $NAME | tr '[A-Z]' '[a-z]')
@@ -42,37 +44,38 @@ ENVNAME="$(echo $NAME | tr '[a-z-]' '[A-Z_]')_ROOT"
 
 echo "Preparing your '$SUBNAME' sub!"
 
-if [ "$NAME" != "sub" ]; then
-  rm bin/sub
+if [[ $NAME != "sub" ]]; then
+    rm bin/sub
 
-  for file in **/sub*; do
-    if [ -d "$file" ]; then
-      prepare_sub $file $SUBNAME
-    elif [ -f "$file" ]; then
-      create_sub $file $SUBNAME $ENVNAME
-    fi
-  done
+    for file in **/sub-*; do
+      prepare_sub "$file"
+    done
 
-  chmod_files "libexec"
+    chmod_files "libexec"
 
-  ln -s ../libexec/$SUBNAME bin/$SUBNAME
+    ln -s ../libexec/$SUBNAME bin/$SUBNAME
 fi
 
 rm README.md
 rm prepare.sh
 
-echo "Done! Enjoy your new sub! If you're happy with your sub, run:"
-echo
-echo "    rm -rf .git"
-echo "    git init"
-echo "    git add ."
-echo "    git commit -m 'Starting off $SUBNAME'"
-echo "    ./bin/$SUBNAME init"
-echo
-echo "Made a mistake? Want to make a different sub? Run:"
-echo
-echo "    git add ."
-echo "    git checkout -f"
-echo
-echo "Thanks for making a sub!"
+cat << DONEMSG
+Done! Enjoy your new sub! If you're happy with your sub, run:
 
+    rm -rf .git
+    git init
+    git add .
+    git commit -m 'Starting off $SUBNAME'
+    ./bin/$SUBNAME init
+
+You can remove the example when you no longer need it:
+
+   rm libexec/${SUBNAME}-example
+
+Made a mistake? Want to make a different sub? Run:
+
+    git add .
+    git checkout -f
+
+Thanks for making a sub!
+DONEMSG
